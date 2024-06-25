31500810c596ae40f4bace4f3c11ad5b8c9729e8
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2024-06-25 12:53:24 +0200

    m´s

--- proyectos/webapp/src/components/usuario/usuario.component.ts
+++ proyectos/webapp/src/components/usuario/usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'usuario',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
 })
-export class UsuarioComponent {
+export class UsuarioComponent /*implements OnInit*/ { 
+                              // En versiones anteriores de Angular era oblitorio hacer esta implementación
+                              // para que se ejecutara el método ngOnInit()
+                              // Hoy en día, Angular ya lo hace por nosotros
+  // Estas propiedades que definimos aquí se guardan a nivel de cada instancia de este componente
+  // En automático, en cuanto una propiedad definida a nivel de mi componente cambie,
+  // Angular se encarga de actualizar la vista. Reejecuta la plantilla.
+  // Internamente eso se hace invocando el método render() de mi componente.
+  nombre: string = 'Juan'; 
+  edad: number = 0;
+
+  constructor() { 
+    // Cada vez que se use la marca HTML <usuario>, 
+    // Angular instanciará un new UsuarioComponent()
+    // Y al hacerlo, se llama en automático al constructor
+    console.log('Se ha instanciado el componente Usuario '+ this.nombre);
+  }
+
+  ngOnInit() {
+    console.log('Se ha iniciado el componente Usuario ' + this.nombre);
+  }
+
+  incrementarEdad() {
+    this.edad++;
+  }
 
 }
